e945fcec4412b8b980f2b8bd7b9f2e2e47b085df
Aryansukhadia <126433929+Aryansukhadia@users.noreply.github.com>
2024-07-22 12:28:01 +0530

    Delete elevate directory

--- elevate/crm/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('index',views.home,name='home'),
-    path('register',views.register,name='register'),
-    path('',views.my_log,name='my_log'),
-    path('dashboard',views.dashboard,name='dashboard'),
-    path('user_logout',views.user_logout,name='user_logout'),
-]
