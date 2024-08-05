01c2be22b7d36ebe1ebc530713158c0cd6685017
Rahil Patel <rahilpatel2474@gmail.com>
2024-08-05 17:24:01 +0530

    update

--- elevate/crm/templates/custom_filters.py
+++
@@ -1,8 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter(name='add_placeholder')
-def add_placeholder(field, placeholder_text):
-    field.field.widget.attrs.update({'placeholder': placeholder_text})
-    return field
