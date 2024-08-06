668f8a02073fc5dc1d9ff2415d46d5ecae0a16ed
Rahil Patel <rahilpatel2474@gmail.com>
2024-08-06 23:04:59 +0530

    new commit

--- elevate/elevate/urls.py
+++ elevate/elevate/urls.py
@@ -1,16 +1,19 @@
-
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from crm import views
 
+# Defining the URL patterns for the project.
 urlpatterns = [
+    # URL pattern for the admin site.
     path("admin/", admin.site.urls),
-    # path("accounts/",include("allauth.urls")),
-    path("",include("crm.urls")),
-
+    # Including the URL patterns from the 'crm' application.
+    path("", include("crm.urls")),
 ]
 
-urlpatterns += static(settings.STATIC_URL,document_root = settings.STATIC_ROOT)
+# Serving static files during development.
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serving media files during development.
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- elevate/elevate/wsgi.py
+++ elevate/elevate/wsgi.py
@@ -7,23 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elevate.settings")
-
-# application = get_wsgi_application()
-
-# app = application
-
 import os  # Importing the os module to interact with the operating system.
-
 from django.core.wsgi import get_wsgi_application  # Importing get_wsgi_application to get the WSGI application.
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 # Setting the default settings module for the 'elevate' project.
 # 'elevate.settings' refers to the settings.py file in the elevate project directory.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elevate.settings")
@@ -32,6 +18,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elevate.settings")
 # This instance will be used to handle WSGI requests.
 application = get_wsgi_application()
 
-# Assigning the WSGI application instance to a variable named 'app'.
-# This can be useful for some WSGI servers or configurations that expect 'app'.
 
