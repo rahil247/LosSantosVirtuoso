7e327dee80b2ddb2f3f0d110cebcc522ab8fff7d
Aryan Sukhadia <126433929+Aryansukhadia@users.noreply.github.com>
2025-07-28 23:34:53 +0530

    Update settings.py

--- elevate/elevate/settings.py
+++ elevate/elevate/settings.py
@@ -102,11 +102,11 @@ DATABASES = {
         "HOST": "mysql-3025ffe1-aryansukhadia2511-10f2.j.aivencloud.com",  # Database host.
         # "PORT": "12277",  # Database port.
         "PORT": "21878",  # Corrected port
-        "OPTIONS": {
-            "ssl": {
-                "ca": "/path/to/ca.pem"  # Only if Aiven requires SSL cert
-            }
-        }
+        # "OPTIONS": {
+        #     "ssl": {
+        #         "ca": "/path/to/ca.pem"  # Only if Aiven requires SSL cert
+        #     }
+        # }
     }
 }
 
